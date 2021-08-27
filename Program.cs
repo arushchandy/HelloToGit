a2d7c5fb2a98df4d78c029c22c188aacdcaf5c96
Milan Harindran <44251616+milanharindran566@users.noreply.github.com>
2021-08-27 15:24:54 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+
+namespace GitSampleConsole
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World Anjana!");
+            Console.WriteLine("Hello World Milan");
+
+            PrintFrom1To10();
+
+            Console.ReadLine();
+        }
+
+        private static void PrintFrom1To10()
+        {
+            var i = 0;
+            while (i < 10)
+            {
+                Console.WriteLine(i);
+                i += 1;
+                Thread.Sleep(100);
+            }
+        }
+    }
+}
